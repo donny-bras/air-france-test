070aa32126faca31bf110ec9b3b8c245258c276a
donny-bras <Serhii.Unhurian@nordstrom.com>
2023-06-28 13:28:55 +0300

    refac: update RepositoryItem props type

--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -1,12 +1,23 @@
 import { ListItem, ListItemButton, ListItemText, Rating } from '@mui/material';
 import { Repository } from '../models/Repository';
 
-interface RepositoryItemProps extends Repository {
+type WithRatingProps = {
+  rating: number | null;
+  onRatingChange: (repoId: string, newRating: number | null) => void;
+};
+
+type WithoutRatingProps = {
+  rating?: never;
+  onRatingChange?: never;
+};
+
+type CommonProps = {
   style?: React.CSSProperties;
   action: React.ReactNode;
-  rating?: number | null;
-  onRatingChange?: (repoId: string, newRating: number | null) => void;
-}
+};
+
+type RepositoryItemProps = Repository &
+  CommonProps & (WithRatingProps | WithoutRatingProps);
 
 const RepositoryItem = ({
   id,
@@ -36,7 +47,7 @@ const RepositoryItem = ({
           <Rating
             size="small"
             value={rating}
-            onChange={(_, newValue) => onRatingChange?.(id, newValue)}
+            onChange={(_, newValue) => onRatingChange(id, newValue)}
             sx={{ mr: 1 }}
           />
         )}
